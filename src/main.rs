0d858bd403e254dcbf07fd16dc2ab3f00c16b368
Thai Anh Duc <anhduc.thai@gmail.com>
2022-07-16 15:49:48 +0700

    More rust examples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io;
+use std::{io, cmp::Ordering};
 use rand::Rng;
-use std::cmp::Ordering;
+
+//pub mod ty
 
 fn main() {
     println!("Hello, Rust!");
@@ -28,10 +29,46 @@ fn main() {
 
     let learn = LearnRust::Option(10);
     execute(learn);
+
+    let mut array: Vec<i32> = Vec::new();
+    array.push(1);
+    array.push(4);
+
+    let second: i32 = array[1];
+    println!("Array at 1 index {}", second);
+    let second_again: i32 = array[1];
+    println!("Array at 1 index again {}", second_again);
+    println!("Array at 1 index work? {}", second);
+
+    let vec_macro = vec![String::from("Hello"), String::from("Reference type")];
+    let hell = &vec_macro[0];
+    println!("Hello hell cool: {}", hell);
+
+
+    println!("Playing around with generics");
+    let numbers = [2, 3, 1, 8];
+    let largest = largest(&numbers);
+
+    println!("Largest number {}", largest);
 }
 
-fn execute(learn: LearnRust)
-{
+/// Find the largest value in a list
+fn largest<T : PartialOrd>(list: &[T]) -> &T {
+    let mut index: usize = 0;
+    let mut largest_value_index: usize = 0;
+
+    while index < list.len() - 1 {
+        if list[index+1] > list[index]  {
+            largest_value_index = index + 1;
+        }
+        index = index + 1;
+    }
+    
+
+    &list[largest_value_index]
+}
+
+fn execute(learn: LearnRust){
     match learn {
         LearnRust::RandomGame => random_game(),
         LearnRust::Option(start) => {
---
+++ src/types/struct_option.rs
@@ -0,0 +1,6 @@
+pub(crate) fn plus_one(x: Option<i32>) -> Option<i32>{
+    match x {
+        None => None,
+        Some(i) => Some(i+1)
+    }
+}
